0da666317a805bb9303cc3d8df5c48e5e14e74af
Ali-Asghar Muhammadi <aamu@kth.se>
2025-07-18 22:17:19 +0200

    Har utvidgad koden så att den kan sortera en lista som är inte sorterad.

--- insätningssortering.py
+++ insätningssortering.py
@@ -13,8 +13,21 @@ def insert_in_serted(x, sorted_list):
      new_list.append(x)
    return new_list
 
+def insertion_sort(my_list):
+   out = []
+   for x in my_list:
+       out = insert_in_serted(x, out)
+   return out
+
 
 print(insert_in_serted(2, []))
 print(insert_in_serted(5, [1, 2, 3, 4]))
 print(insert_in_serted(2, [0, 1, 2, 3, 4]))
 print(insert_in_serted(2, [2, 2]))
+
+
+print(insertion_sort([4, 3, 2, 1]))
+print(insertion_sort([5, 1, 4, 2, 8]))
+print(insertion_sort([1, 1, 1, 1]))
+print(insertion_sort([]))
+print(insertion_sort([12, 4, 3, -1]))
